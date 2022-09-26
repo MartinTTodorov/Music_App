ed9ee9ff5a61a9559725d4a59b4e73bdf197ea23
Martin Todorov <i481796@fhict.nl>
2022-09-26 21:49:35 +0200

    project

---
+++ music_individual/src/main/java/music_individual/demo/MusicIndividualApplication.java
@@ -0,0 +1,13 @@
+package music_individual.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicIndividualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicIndividualApplication.class, args);
+	}
+
+}
---
+++ music_individual/src/test/java/music_individual/demo/MusicIndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package music_individual.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicIndividualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
